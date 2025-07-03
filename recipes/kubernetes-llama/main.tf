bb2f6a848bb23334aeb61c90be5d762a467674bd
Reshma Abdul Rahim <reshmarahim.abdul@microsoft.com>
2025-07-03 16:40:43 -0400

    Upd
    Signed-off-by: Reshma Abdul Rahim <reshmarahim.abdul@microsoft.com>

--- recipes/kubernetes-llama/main.tf
+++ recipes/kubernetes-llama/main.tf
@@ -99,7 +99,7 @@ resource "kubernetes_service" "llama" {
 output "result" {
   value = {
     values = {
-        llama_endpoint = "http://${kubernetes_service.llama.metadata[0].name}.${kubernetes_namespace.llama.metadata[0].name}.svc.cluster.local:8080"
+        llama_endpoint = "http://${kubernetes_service.llama.metadata[0].name}.${kubernetes_service.llama.metadata[0].namespace}.svc.cluster.local:8080"
     }
   }
 }
